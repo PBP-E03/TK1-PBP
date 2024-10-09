8aeccdf565de2551e85ba35e1ad0dd66f69fc5f3
Danniel-Ang <danniel@ui.ac.id>
2024-10-09 19:52:26 +0700

    Fixing bug, renaming and proof read README

--- reservation/apps.py
+++ reservation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ForumDiscussionConfig(AppConfig):
+class ReservationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'forum_discussion'
+    name = 'reservation'
