045a8a9c16d2609d5a83a39227cc58f4fdf1835d
Danniel-Ang <danniel@ui.ac.id>
2024-10-23 00:05:11 +0700

    Adding user authorization

--- main/views.py
+++ main/views.py
@@ -1,5 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Serializer
+from django.core import serializers
+
+# Auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+# Date for Session
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# CSRF and Security
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 # Create your views here.
+@login_required(login_url='main:user_login')
 def main_page(request):
     return render(request, 'main_page.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:main_page"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def user_register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:user_login')
+        else :
+            messages.error(request, 'Error creating your account')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def user_logout(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:user_login'))
+    response.delete_cookie('last_login')
+    return response
