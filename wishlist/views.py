3e141a2f277986079fa267f7bebe78ba665e3108
Nafisa <nafisa.arrasyida@gmail.com>
2024-12-22 04:11:20 +0700

    solved conflicts

--- wishlist/views.py
+++ wishlist/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 import json
+from django.core import serializers
 
 def view_wishlists(request):
     categorized_wishlists = {}
