b05a212af00e96f1c75549259c162cac4014141c
anitakny <antkhrnns9@gmail.com>
2024-12-22 19:11:58 +0700

    tambah POST json

--- reservation/urls.py
+++ reservation/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('edit_reservation/<int:reservation_id>/', edit_reservation, name='edit_reservation'),
     path('delete_reservation/<int:reservation_id>/', delete_reservation, name='delete_reservation'),
 
+    
     path('api/reservations/create/', create_reservation, name='create_reservation'),
     path('reservation/flutter/edit-reservation/', edit_reservation, name='edit_reservation'),
     path('reservation/flutter/delete-reservation/', delete_reservation, name='delete_reservation'),
